a758fa61211f03044d64da2c53e7ca5d751238c6
Hiram Vazquez Almeida <hvazqueza@ia.com.mx>
2025-02-17 14:40:11 -0600

    Initial Commit

---
+++ Catalog/CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogApp.swift
+//  Catalog
+//
+//  Created by Hiram Vázquez Almeida on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Catalog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Catalog
+//
+//  Created by Hiram Vázquez Almeida on 17/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
